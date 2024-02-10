414a112815f12fa414d35d218b588264457d9412
RSGuelfi <rosantosguelfi@gmail.com>
2024-02-10 12:46:57 -0300

    2

--- handlers/update.go
+++ handlers/update.go
@@ -7,10 +7,11 @@ import (
 	"strconv"
 
 	"github.com/RSGuelfi/models"
+	"github.com/go-chi/chi/v5"
 )
 
 func Update(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(char.URLParam(r, "id"))
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
 	if err != nil {
 		log.Printf("Erro ao fazer parse do id: %v", err)
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
